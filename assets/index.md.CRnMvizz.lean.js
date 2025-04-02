a3cd4ec2e7b4e2510bd28244973e22e42cf1a9cb
Zhang Henghua <1652709417@qq.com>
2025-04-02 16:45:09 +0800

    update notes

---
+++ assets/index.md.CRnMvizz.lean.js
@@ -0,0 +1,19 @@
+import{_,c as e,o as t}from"./chunks/framework.BZemHgQ6.js";console.log(`
+ _______
+ |___  / |
+    / /| |__   __ _ _ __   __ _ 
+   / / | '_ \\ / _\` | '_ \\ / _\` |
+  / /__| | | | (_| | | | | (_| |
+ /_____|_| |_|\\__,_|_| |_|\\__, |
+ | |  | |                  __/ |
+ | |__| | ___ _ __   __ _ |___/
+ |  __  |/ _ \\ '_ \\ / _\` |
+ | |  | |  __/ | | | (_| |
+ |_|  |_|\\___|_| |_|\\__, |
+ | |  | |            __/ |
+ | |__| |_   _  __ _|___/
+ |  __  | | | |/ _\` |
+ | |  | | |_| | (_| |
+ |_|  |_|\\__,_|\\__,_|
+                        @ 张恒华
+`);const p=JSON.parse('{"title":"","description":"","frontmatter":{"layout":"home","hero":{"name":"张恒华","text":"技术爱好者","tagline":"SDN, OpenFlow, Mininet, Ryu, P4, gRPC","image":{"src":"/typewriter.svg","alt":"Typewriter"},"actions":[{"theme":"brand","text":"学习笔记","link":"/sdn/"},{"theme":"alt","text":"关于我","link":"/resume"}]},"features":[{"icon":{"src":"/p4/p4-logo.svg","width":30},"title":"P4","details":"Programming Protocol-independent Packet Processors","link":"/sdn/p4","linkText":"查看笔记"},{"icon":{"src":"/mininet/favicon.png","width":26},"title":"Mininet","details":"An Instant Virtual Network","link":"/sdn/mininet","linkText":"查看笔记"},{"icon":{"src":"/go/grpc.png","width":28},"title":"gRPC","details":"A High-Performance, Open-Source Universal RPC Framework","link":"/go/grpc","linkText":"查看笔记"}]},"headers":[],"relativePath":"index.md","filePath":"index.md"}'),n={name:"index.md"};function i(r,o,a,s,c,l){return t(),e("div")}const m=_(n,[["render",i]]);export{p as __pageData,m as default};
