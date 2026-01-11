b8b7ed3876119dcf8ddcc5a80b0e81fc356a2468
Henghua Zhang <1652709417@qq.com>
2026-01-11 17:04:15 +0800

    docs(config): update github repo link and clean up keywords
    Update the editLink pattern to point to the correct repository and remove redundant keywords from the config file to keep it concise

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,9 +1,5 @@
 import { defineConfig } from 'vitepress'
 
-const siteMapIcon = {
-  svg: '<svg t="1760360619836" class="icon" viewBox="0 0 1280 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="3431" width="64" height="64"><path d="M256 704H64c-35.34 0-64 28.66-64 64v192c0 35.34 28.66 64 64 64h192c35.34 0 64-28.66 64-64v-192c0-35.34-28.66-64-64-64z m-48-160h384v96h96v-96h384v96h96v-115.18c0-42.34-34.46-76.82-76.82-76.82H688v-128h80c35.34 0 64-28.66 64-64V64c0-35.34-28.66-64-64-64H512c-35.34 0-64 28.66-64 64v192c0 35.34 28.66 64 64 64h80v128H188.82C146.46 448 112 482.46 112 524.82V640h96v-96z m528 160h-192c-35.34 0-64 28.66-64 64v192c0 35.34 28.66 64 64 64h192c35.34 0 64-28.66 64-64v-192c0-35.34-28.66-64-64-64z m480 0h-192c-35.34 0-64 28.66-64 64v192c0 35.34 28.66 64 64 64h192c35.34 0 64-28.66 64-64v-192c0-35.34-28.66-64-64-64z" p-id="3432" fill="#7F7F85"></path></svg>'
-}
-
 const SDNNoteItems = [
   { text: 'P4 Language', link: '/sdn/p4' },
   { text: 'P4 Exercise', link: '/sdn/p4_exercise' },
@@ -54,11 +50,7 @@ const TechNotes = [
 
 const keywords = [
   '张恒华',
-  '笔记', '学习笔记', '科研笔记', '读书笔记', '阅读笔记',
-  '文献阅读', '论文阅读',
   '软件定义网络', '可编程数据平面', '带内网络遥测',
-  'Go语言', 'Go协程', 'Go开发',
-  'SDN控制器',
   'Henghua Zhang',
   'SDN', 'Software Defined Network', 'Software-Defined Networking',
   'Programmable Data Plane', 'PDP',
@@ -71,7 +63,6 @@ const keywords = [
   'Mininet',
   'iPerf',
   'Scapy',
-  'Ryu',
   'Go', 'GoLang',
   'RPC', 'gRPC',
   'Goroutine', 'GMP',
@@ -124,7 +115,7 @@ export default defineConfig({
   themeConfig: {
     siteTitle: '张恒华',
     editLink: {
-      pattern: ({ filePath }) => `https://github.com/zhh2001/notes/tree/main/docs/${filePath}`
+      pattern: ({ filePath }) => `https://github.com/zhh2001/zhh2001.github.io/tree/main/docs/${filePath}`
     },
     footer: {
       message: 'Released under the <a href="/mit">MIT License</a>.',
